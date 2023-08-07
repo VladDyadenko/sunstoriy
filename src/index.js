7180bf194773c99324d9b06f079bf04e6dcaf8a3
Vladimir Dyadenko <Stilmagroup@gmail.com>
2023-08-07 20:15:28 +0300

    daploy

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import GlobalStyles from './styles/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="sunstoriy">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
