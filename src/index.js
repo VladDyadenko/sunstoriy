47866cfeb203ed28b69ab3e6a3ced1298412ec44
Vladimir Dyadenko <Stilmagroup@gmail.com>
2023-08-07 20:12:39 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyles from './styles/GlobalStyles';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter basename="sunstoriy">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
