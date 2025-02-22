9b6f7dc7dc05bdda20107911f946b073e34ba39f
VladDyadenko <110708917+VladDyadenko@users.noreply.github.com>
2025-02-22 13:44:18 +0000

    Deploying to gh-pages from @ VladDyadenko/sunstoriy@ec5bd8769d451d72bc428bed0afb1e670ad21e77 🚀

--- static/js/629.dd6d7fd6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksunstoriy=self.webpackChunksunstoriy||[]).push([[629],{6629:function(n,s,u){u.r(s);var e=u(6757),r=u(184);s.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(e.Z,{})})}}}]);
-//# sourceMappingURL=629.dd6d7fd6.chunk.js.map
